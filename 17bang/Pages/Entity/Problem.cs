6de0147d48611294e767bd54909ae0af0a568529
王帅 <1685909098@qq.com>
2020-04-25 18:33:11 +0800

    Add project files.

---
+++ 17bang/Pages/Entity/Problem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bang.Pages.Entity
+{
+    public class Problem
+    {
+        public DateTime PublishTime;
+        public User Author;
+        public string Title;
+        public int Id;
+        public string Abstact;
+        public ProblemStatus  Status;
+    }
+    public enum ProblemStatus
+    {
+        [Description("已撤销")]
+        Cancelled,
+        [Description("待协助")]
+        WaitingProcess,
+        [Description("协助中")]
+        InProcess,
+        [Description("已酬谢")]
+        Rewarded,
+    }
+}
---
+++ 17bang/Pages/Entity/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bang.Pages.Entity
+{
+    public class User
+    {
+        public string Name;
+        public int Id;
+    }
+}
