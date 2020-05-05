15667c643872f636d65160ca1f9c8073531fad9f
王帅 <1685909098@qq.com>
2020-05-05 18:53:40 +0800

    session过期时间练习   了解到浏览器的缓存问题

---
+++ 17bang/Pages/Log/On.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using _17bang.Pages.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17bang.Pages.Log
+{
+    public class OnModel : PageModel
+    {
+        public User User { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
