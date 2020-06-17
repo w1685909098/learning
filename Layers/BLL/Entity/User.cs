61f86fbe1b31670a4aaf4b3c5266ca7c449a7977
王帅 <1685909098@qq.com>
2020-06-18 06:52:28 +0800

    三层架构的分层

---
+++ Layers/BLL/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class User
+    {
+        public User Inviter { get; set; }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string InvitingCode { get; set; }
+    }
+}
---
+++ Layers/BLL/Repository/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserRepository
+    {
+        private IList<User> _user;
+        public User GetUserByName(string name)
+        {
+            return _user.Where(u => u.UserName == name).SingleOrDefault();
+        }
+    }
+}
