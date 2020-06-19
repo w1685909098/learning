d943742e481ccc90fb64513b00fadc5ad0057909
王帅 <1685909098@qq.com>
2020-06-19 12:12:44 +0800

    MVC进行model绑定

--- Layers/UI/WebUI/Controllers/RegisterController.cs
+++ Layers/UI/WebUI/Controllers/RegisterController.cs
@@ -2,9 +2,11 @@
 using Repository;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
 
 namespace WebUI.Controllers
 {
@@ -38,8 +40,13 @@ namespace WebUI.Controllers
 
     public class IndexModel
     {
+        [Required]
         public User Inviter { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [MaxLength(18,ErrorMessage ="* 密码最大长度不能超过18")]
         public string Password { get; set; }
+        [Compare(nameof(Password),ErrorMessage ="* 两次输入密码不一致")]
+        public string ComfirmPassword { get; set; }
     }
 }
