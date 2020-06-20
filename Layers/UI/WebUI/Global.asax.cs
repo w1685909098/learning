9b6b730dc22c2b49de0082674741e1640e6ed6b1
王帅 <1685909098@qq.com>
2020-06-20 19:21:47 +0800

    autofac的引用及单个register的配置

--- Layers/UI/WebUI/Global.asax.cs
+++ Layers/UI/WebUI/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.UI.WebControls;
 
 namespace WebUI
 {
@@ -16,6 +19,17 @@ namespace WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ContainerBuilder builder = new ContainerBuilder(); //生成builder
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);  //注册controller
+            builder.RegisterFilterProvider();                  //注册filter
+
+            builder.RegisterType<ProdService.RegisterService>().As<ServiceInterface.IRegisterService>();   //单个类注册
+            builder.RegisterAssemblyTypes(typeof(ProdService.RegisterService).Assembly).AsImplementedInterfaces();   //项目集合注册
+
+
+            IContainer container= builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
