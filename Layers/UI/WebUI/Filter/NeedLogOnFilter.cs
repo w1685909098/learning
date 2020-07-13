483b5594f2d828ac1023af7d0209502005481feb
王帅 <1685909098@qq.com>
2020-07-13 21:11:23 +0800

    filter的引入

---
+++ Layers/UI/WebUI/Filter/NeedLogOnFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Filter
+{
+    public class NeedLogOnFilter :AuthorizeAttribute/*,IAuthorizationFilter,IActionFilter,IResultFilter,IExceptionFilter*/
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+      
+    }
+}
